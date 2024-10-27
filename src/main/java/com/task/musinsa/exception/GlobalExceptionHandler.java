6b6d751b96de2eece42165184fe22da096b53756
ch200203 <ch200205@naver.com>
2024-10-27 23:57:47 +0900

    feat : CustomException 정리

--- src/main/java/com/task/musinsa/exception/GlobalExceptionHandler.java
+++ src/main/java/com/task/musinsa/exception/GlobalExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    
 
     @ExceptionHandler(DuplicateNameException.class)
     public ResponseEntity<CustomErrorResponse> handleDuplicateNameException(DuplicateNameException ex) {
